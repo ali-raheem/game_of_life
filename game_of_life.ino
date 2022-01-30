b778ef23858e50ec1ac183b597a6a8d183f93ace
Ali Raheem <ali-windows@shoryuken.me>
2022-01-30 15:34:27 +0000

    Fix sign extension bug

--- game_of_life.ino
+++ game_of_life.ino
@@ -1,20 +1,49 @@
+// Copyright 2022 Ali Raheem <github@shoryuken.me>
+
 const int ROWS = 32;
 const int COLS = 32; // bits in type used for state array unsigned long
 bool active = false;
 unsigned long state[2][ROWS];
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Game of Life - Arduino 32x32");
-  flip();
-  randomize();
-}
+bool get_state_wrapped(int, int);
+bool get_state_closed(int, int);
 
-bool get_state (int i, int j) {
+bool (*get_state)(int, int) = get_state_wrapped;
+
+// Closed topology
+bool get_state_closed (int i, int j) {
   if (i < 0 || i > (ROWS-1) || j < 0 || j > (COLS-1))
     return false;
   return ((state[active][i]) >> j) & 1;
 }
+
+// Wrapped topology
+bool get_state_wrapped (int i, int j) {
+  if (i < 0)
+    i = ROWS - 1;
+  if (i > (ROWS - 1))
+    i = 0;
+  if (j < 0)
+    j = COLS - 1;
+  if (j > (COLS - 1))
+    j = 0;
+  return (state[active][i] >> j) & 1;
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Game of Life - Arduino 32x32");
+  // flip will initialise the frame buffers to 0
+  // randomize will intialise them... randomly.
+  flip();
+  // flip();
+  randomize();
+  // Setting a glider to start with.
+  state[active][15] = 0x00020000; // Glider
+  state[active][16] = 0x00010000; //
+  state[active][17] = 0x00070000; //
+}
+
 
 int sum (int i, int j) {
   int s = get_state(i - 1, j - 1) + get_state(i - 1, j) + get_state(i - 1, j + 1) +
@@ -26,14 +55,14 @@ int sum (int i, int j) {
 void update_state (int i, int j) {
   switch (sum(i, j)) {
     case 3:
-      state[!active][i] |= 1 << j;
+      state[!active][i] |= (1UL << j);
       break;
     case 4:
-      int s = get_state(i, j);
+      unsigned long s = get_state(i, j);
       state[!active][i] |= (s << j);
       break;
     default:
-      state[!active][i] &= (0xff - (1 << j));
+      state[!active][i] &= ~(1UL << j);
   }
 }
 
@@ -48,10 +77,10 @@ void randomize() {
    int i;
    active = !active;
    for(i = 0; i < ROWS; i++)
-    state[active][i] =  analogRead(0) << 24 |
-                        analogRead(0) << 16 |
-                        analogRead(0) << 8  | 
-                        analogRead(0);
+    state[active][i] =  (unsigned long) analogRead(0) << 24 |
+                        (unsigned long) analogRead(0) << 16 |
+                        (unsigned long) analogRead(0) << 8  | 
+                        (unsigned long) analogRead(0);
 }
 
 void loop() {
@@ -60,9 +89,9 @@ void loop() {
     for (j = 0; j < COLS; j++) {
       bool s = get_state(i, j);
       if (s) {
-        Serial.print(" X ");
+        Serial.print(" + ");
       } else {
-        Serial.print(" O ");
+        Serial.print(" - ");
       }
       update_state(i, j);
     }
@@ -71,5 +100,5 @@ void loop() {
   Serial.println("");
 
   flip();
-  delay(500);
+ delay(50);
 }
