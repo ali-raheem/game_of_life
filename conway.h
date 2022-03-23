5d24f333536c8b1dcfd320e43e92c50c716aa834
Ali Raheem <ali.raheem@gmail.com>
2022-03-23 21:42:02 +0000

    Add optimization switch for larger matricies

--- conway.h
+++ conway.h
@@ -2,7 +2,7 @@
 Conway is a library for running John Conway's Game of Life
 Copyright Ali Raheem 2022 - https://github.com/ali-raheem/conway
 MIT Licensed
-File version: 22/03/2022 17:39 GMT
+File version: 2022-03-23 21:41 GMT
 */
 
 #ifndef CONWAY_H
@@ -30,10 +30,8 @@ class Conway {
     uint16_t next();
     void clear();
     bool getNextCellState (bool s, uint8_t sum);
-    void updateCellState (uint16_t i, uint16_t j, uint32_t s, uint8_t sum);
     bool getCellState (int8_t i, int8_t j);
   private:
-    uint8_t activeLineBuffer;
     uint8_t lastPopulation;
 };
 
@@ -66,21 +64,6 @@ void Conway<T>::clear() {
     state[i] = 0;
 }
 
-
-template <class T>
-void Conway<T>::updateCellState (uint16_t i, uint16_t j, uint32_t s, uint8_t sum) {
-  const T one = 1;
-  switch (sum) {
-    case 3:
-      state[activeLineBuffer] |= (one << j);
-      break;
-    case 4:
-      state[activeLineBuffer] |= (s << j);
-      break;
-    default:
-      state[activeLineBuffer] &= ~(one << j);
-  }
-}
 template <class T>
 bool Conway<T>::getNextCellState (bool s, uint8_t sum) {
   switch (sum) {
@@ -96,38 +79,62 @@ bool Conway<T>::getNextCellState (bool s, uint8_t sum) {
 }
 template <class T>
 uint16_t Conway<T>::next() {
-  uint16_t i, j;
   population = 0;
-  activeLineBuffer = rows + 2;
-  for (i = 0; i < rows; i++) {
+  uint8_t activeLineBuffer = rows + 2;
+  for (uint8_t i = 0; i < rows; i++) {
     state[activeLineBuffer] = 0;
-    uint8_t sum_l = getCellState(i - 1, -1) + getCellState(i, -1) + getCellState(i + 1, -1);
-    bool oldState = getCellState(i, 0);
-    uint8_t sum_m = getCellState(i - 1, 0) + oldState + getCellState(i + 1, 0);
+    T prevRow = state[(i == 0)? rows - 1 : i - 1];
+    T currRow = state[i];
+    T nextRow = state[(i + 1) % rows];
+    uint8_t sum_l = !!(prevRow & (1 << (sizeof(T)*8 - 1))) + !!(currRow & (1 << (sizeof(T)*8 - 1))) + !!(nextRow & (1 << (sizeof(T)*8 - 1)));
+    bool oldState = !!(currRow & 1);
+    uint8_t sum_m = !!(prevRow & 1) + oldState + !!(nextRow & 1);    
+//    uint8_t sum_l = getCellState(i - 1, -1) + getCellState(i, -1) + getCellState(i + 1, -1);
+//    bool oldState = getCellState(i, 0);
+//    uint8_t sum_m = getCellState(i - 1, 0) + oldState + getCellState(i + 1, 0);
     uint8_t sum_0 = sum_m;
-    uint8_t sum_r;
-    bool oldStateR;
-    uint8_t liveCells;
-    oldStateR = getCellState(i, 1);
-    sum_r = getCellState(i - 1, 1) + oldStateR + getCellState(i + 1, 1);
-    liveCells = sum_l + sum_m + sum_r;
-    state[activeLineBuffer] |= ((T) getNextCellState(oldState, liveCells)) << (sizeof(T)*8 - 1);
+//    uint8_t sum_r;
+//    bool oldStateR;
+//    uint8_t liveCells;
+//    if(prevRow == 0 && currRow == 0 && nextRow == 0)
+//      continue;
+#ifdef __CONWAY_OPTIMIZE_LARGE
+    prevRow >>= 1;
+    currRow >>= 1;
+    nextRow >>= 1;
+    bool oldStateR = !!(currRow & 0b1);
+    uint8_t sum_r = !!(prevRow & 0b1) + oldStateR + !!(nextRow & 0b1);
+#else
+    bool oldStateR = getCellState(i, 1);
+    uint8_t sum_r = getCellState(i - 1, 1) + oldStateR + getCellState(i + 1, 1);
+#endif
+    uint8_t liveCells = sum_l + sum_m + sum_r;
+    if(getNextCellState(oldState, liveCells)) {
+      T one = 1;
+      state[activeLineBuffer] |=  one << (sizeof(T)*8 - 1);
+    }
     population += oldState;
     oldState = oldStateR;
     sum_l = sum_m;
     sum_m = sum_r;
-    T prevRow = state[(i == 0)? rows - 1 : i - 1];
-    T currRow = state[i];
-    T nextRow = state[(i + 1) % rows];
     for (uint8_t j = 1; j < cols - 1; j++) {
       prevRow >>= 1;
       currRow >>= 1;
       nextRow >>= 1;
+ #ifdef __CONWAY_OPTIMIZE_LARGE
+      oldStateR = !!(currRow & 0b1);
+      sum_r = !!(prevRow & 0b1) + oldStateR + !!(nextRow & 0b1);
+ #else
       oldStateR = !!(currRow & 0b10);
       sum_r = !!(prevRow & 0b10) + oldStateR + !!(nextRow & 0b10);
+ #endif
       liveCells = sum_l + sum_m + sum_r;
       state[activeLineBuffer] >>= 1;
-      state[activeLineBuffer] |= ((T) getNextCellState(oldState, liveCells)) << (sizeof(T)*8 - 1);
+      if(getNextCellState(oldState, liveCells)) {
+        T one = 1;
+        state[activeLineBuffer] |=  one << (sizeof(T)*8 - 1);
+      }
+
       population += oldState;
       oldState = oldStateR;
       sum_l = sum_m;
@@ -135,7 +142,10 @@ uint16_t Conway<T>::next() {
     }
     liveCells = sum_l + sum_m + sum_0;
     state[activeLineBuffer] >>= 1;
-    state[activeLineBuffer] |= ((T) getNextCellState(oldState, liveCells)) << (sizeof(T)*8 - 1);
+    if(getNextCellState(oldState, liveCells)) {
+      T one = 1;
+      state[activeLineBuffer] |=  one << (sizeof(T)*8 - 1);
+    }
     population += oldState;
     activeLineBuffer = rows + (i & 1);
     if (LIKELY(i > 1))  state[i - 1] = state[activeLineBuffer];
